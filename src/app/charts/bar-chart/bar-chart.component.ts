8c76e29d723eafbdfa0bb499084dc7398d302f6a
NordiAut <71013884+NordiAut@users.noreply.github.com>
2020-10-06 11:49:42 +0200

    Pie Charts Api Connection

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesDataService } from 'src/app/services/sales-data.service';
+import * as moment from 'moment';
+import { filter } from 'rxjs-compat/operator/filter';
 
-const SAMPLE_BARCHART_DATA: any[] = [
-  { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales'},
-  { data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Sales'}
- ];
+// const SAMPLE_BARCHART_DATA: any[] = [
+//   { data: [65, 59, 80, 81, 56, 54, 30], label: 'Q3 Sales'},
+//   { data: [25, 39, 60, 91, 36, 54, 50], label: 'Q4 Sales'}
+//  ];
 
-const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'];
+// const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'];
 
 @Component({
   selector: 'app-bar-chart',
@@ -14,18 +17,62 @@ const SAMPLE_BARCHART_LABELS: string[] = ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W
 })
 export class BarChartComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  constructor(private _salesDataService: SalesDataService) { }
 
-  public barChartData: any[] = SAMPLE_BARCHART_DATA;
-  public barChartLabels: string[] = SAMPLE_BARCHART_LABELS;
+  orders: any;
+  orderLabels: string[];
+  orderData: number[];
+
+  public barChartData: any[];
+  public barChartLabels: string[];
   public barChartType = 'bar';
   public barChartLegend = true;
   public barChartOptions: any = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-
+  // tslint:disable-next-line:align
   ngOnInit(): void {
+    this._salesDataService.getOrders(1, 100)
+    .subscribe(res => {const localChartData = this.getChartData(res);
+                       this.barChartLabels = localChartData.map(x => x[0]).reverse();
+                       this.barChartData = [{ data: localChartData.map( x => x[1]), label: 'Sales'}];
+    });
+  }
+
+  getChartData(res: any): any {
+
+    this.orders = res.page.data;
+    const data = this.orders.map(o => o.orderTotal);
+
+    // const labels = this.orders.map(o => moment(new Date(o.placed)).format('YY-MM-DD'));
+
+    const formattedOrders = this.orders.reduce((r, e) => {
+      r.push([moment(e.placed).format('YY-MM-DD'), e.orderTotal]);
+      return r;
+    }, []);
+
+    const p = [];
+    const chartData = formattedOrders.reduce((r, e) => {
+    const key = e[0];
+    if (!p[key]) {
+     p[key] = e;
+     r.push(p[key]);
+     }
+     else {
+      p[key][1] += e[1];
+     }
+    return r;
+    }, []);
+
+    return(chartData);
+
+    // const myData = [3, 4, 5].reduce((sum, value) =>
+    // { return sum + value;
+    // }, 0);
+    // console.log('myData:', myData);
+
   }
 
 }
