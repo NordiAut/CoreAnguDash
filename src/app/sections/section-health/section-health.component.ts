8f540501c100d0ca3beeb09cfc0eca1c61abf517
NordiAut <71013884+NordiAut@users.noreply.github.com>
2020-10-07 12:53:27 +0200

    Section Health - Server functionality

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,26 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { Server } from 'src/app/Shared/server';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ServerService } from '../../services/server.service';
+import { Server } from '../../shared/server';
+import { ServerMessage } from '../../shared/server-message';
+// tslint:disable-next-line:import-blacklist
+import { Observable } from 'rxjs/Rx';
+import { AnonymousSubscription } from 'rxjs/Subscription';
 
-const SAMPLE_SERVERS = [
-  { id: 1, name: 'dev-web', isOnline: true },
-  { id: 2, name: 'dev-mail', isOnline: false },
-  { id: 3, name: 'prod-web', isOnline: true },
-  { id: 4, name: 'prod-mail', isOnline: true }
-];
+// const SAMPLE_SERVERS = [
+//   { id: 1, name: 'dev-web', isOnline: true },
+//   { id: 2, name: 'dev-mail', isOnline: false },
+//   { id: 3, name: 'prod-web', isOnline: true },
+//   { id: 4, name: 'prod-mail', isOnline: true }
+// ];
 
 @Component({
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
   styleUrls: ['./section-health.component.css']
 })
-export class SectionHealthComponent implements OnInit {
+export class SectionHealthComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  constructor(private _serverService: ServerService) { }
 
-  servers: Server[] = SAMPLE_SERVERS;
+  servers: Server[];
+  timerSubscription: AnonymousSubscription;
 
   ngOnInit(): void {
+    this.refreshData();
   }
 
+  ngOnDestroy(): void {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
 
+  refreshData(): void {
+    this._serverService.getServers().subscribe(res => {
+      this.servers = res;
+    });
+
+    this.subscribeToData();
+  }
+
+  subscribeToData(): void {
+    this.timerSubscription = Observable.timer(5000).first().subscribe(() => this.refreshData());
+  }
+
+  sendMessage(msg: ServerMessage): void {
+    this._serverService.handleServerMessage(msg)
+      .subscribe(res => console.log('Message sent to server:', msg),
+                 err => console.log('Error:', err));
+  }
 }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,5 +1,4 @@
-import { EventEmitter, Input, Output } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , EventEmitter, Input, Output } from '@angular/core';
 import { Server } from '../Shared/server';
 import { ServerMessage } from '../Shared/server-message';
 
@@ -19,17 +18,16 @@ export class ServerComponent implements OnInit {
 
 
    @Input() serverInput: Server;
-  // @Output() serverAction = new EventEmitter<ServerMessage>();
+   @Output() serverAction = new EventEmitter<ServerMessage>();
 
   ngOnInit(): void {
     this.setServerStatus(this.serverInput.isOnline);
   }
 
-  toggleStatus(onlineStatus: boolean): void {
-    console.log(this.serverInput.name, ':', onlineStatus);
-    this.serverInput.isOnline = !this.serverInput.isOnline;
-    this.setServerStatus(!onlineStatus);
-  }
+  // toggleStatus(onlineStatus: boolean): void {
+  //   this.serverInput.isOnline = !this.serverInput.isOnline;
+  //   this.setServerStatus(!onlineStatus);
+  // }
 
   setServerStatus(isOnline: boolean): void
   {
@@ -50,34 +48,33 @@ export class ServerComponent implements OnInit {
   }
 
 
+  makeLoading(): void  {
+    this.color = '#FFCA28';
+    this.buttonText = 'Pending...';
+    this.isLoading = true;
+    this.serverStatus = 'Loading';
+  }
 
-  // makeLoading(): void  {
-  //   this.color = '#FFCA28';
-  //   this.buttonText = 'Pending...';
-  //   this.isLoading = true;
-  //   this.serverStatus = 'Loading';
-  // }
+  sendServerAction(isOnline: boolean): void  {
+    console.log('sendServerAction called!');
+    this.makeLoading();
+    const payload = this.buildPayload(isOnline);
+    this.serverAction.emit(payload);
+  }
 
-  // sendServerAction(isOnline: boolean): void  {
-  //   console.log('sendServerAction called!');
-  //   this.makeLoading();
-  //   const payload = this.buildPayload(isOnline);
-  //   this.serverAction.emit(payload);
-  // }
-
-  // buildPayload(isOnline: boolean): ServerMessage {
-  //   if (isOnline) {
-  //     return {
-  //       id: this.serverInput.id,
-  //       payload: 'deactivate'
-  //     };
-  //   } else {
-  //     return {
-  //       id: this.serverInput.id,
-  //       payload: 'activate'
-  //     };
-  //   }
-  // }
+  buildPayload(isOnline: boolean): ServerMessage {
+    if (isOnline) {
+      return {
+        id: this.serverInput.id,
+        payload: 'deactivate'
+      };
+    } else {
+      return {
+        id: this.serverInput.id,
+        payload: 'activate'
+      };
+    }
+  }
 
 
 }
